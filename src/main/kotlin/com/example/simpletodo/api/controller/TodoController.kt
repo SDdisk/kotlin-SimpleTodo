bdbb50e04bf803462375d47c449134656679c88d
Artem Rynochnov <TemiX002@mail.ru>
2025-09-19 11:41:39 +0400

    Refactor.

--- src/main/kotlin/com/example/simpletodo/api/controller/TodoController.kt
+++ src/main/kotlin/com/example/simpletodo/api/controller/TodoController.kt
@@ -29,7 +29,7 @@ class TodoController(
     @GetMapping("/todos")
     @ResponseStatus(HttpStatus.OK)
     fun getAll(@PageableDefault(page = 0, size = 5, sort = ["id"], direction = Sort.Direction.DESC) page: Pageable): List<TodoDto> {
-        log.info("CONTROLLER | path: '.../todos', method: 'GET'")
+        log.info("CONTROLLER | path: '.../todos', method: 'GET', page:'number=${page.pageNumber}, size=${page.pageSize}, sortBy=${page.sort}'")
 
         return todoService.getAll(page)
     }
