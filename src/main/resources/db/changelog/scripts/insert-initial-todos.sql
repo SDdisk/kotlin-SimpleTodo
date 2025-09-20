f925eb695014e7ac50c40ee03a62a3e09e01f551
Artem Rynochnov <TemiX002@mail.ru>
2025-09-20 11:24:56 +0400

    Small refactor.

--- src/main/resources/db/changelog/scripts/insert-initial-todos.sql
+++ src/main/resources/db/changelog/scripts/insert-initial-todos.sql
@@ -1,9 +1,9 @@
 INSERT INTO todo_table (title, description, is_completed) VALUES
-                                                              ('Create SQL-script for inserting initial todos', 'description lol', true),
-                                                              ('Understand Spring', 'Unreachable', false),
-                                                              ('Find first work', 'PLEASE', false),
-                                                              ('Link database with kotlin-project and IDE', 'IDE: IntelliJ IDEA. DB: PostgreSQL.', true),
-                                                              ('Create repository', 'Link database with business logic', true),
-                                                              ('Create business logic', 'aka service', true),
-                                                              ('Create rest-controller', 'Autowire service', true),
-                                                              ('Learn: How to create the tests', '???', false);
+('Create SQL-script for inserting initial todos', 'description lol', true),
+('Understand Spring', 'Unreachable', false),
+('Find first work', 'PLEASE', false),
+('Link database with kotlin-project and IDE', 'IDE: IntelliJ IDEA. DB: PostgreSQL.', true),
+('Create repository', 'Link database with business logic', true),
+('Create business logic', 'aka service', true),
+('Create rest-controller', 'Autowire service', true),
+('Learn: How to create the tests', '???', false);
